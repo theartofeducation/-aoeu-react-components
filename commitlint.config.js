159312837bab6aed4af34683387f1043d61ced22
Bob Yexley <bob@yexley.net>
2020-11-12 12:45:46 -0500

    wip: extended commitlint configuration

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,25 @@
-module.exports = {
-  extends: ["@commitlint/config-conventional"]
-}
+const conventionalConfig = require("@commitlint/config-conventional")
+const { rules: defaultRules } = conventionalConfig
+
+/**
+ * Extend "@commitlint/config-conventional" with two adjustments to
+ * types allowed: change "feat" to "feature", and add "wip"
+ */
+module.exports = Object.assign({}, conventionalConfig, {
+  rules: Object.assign({}, defaultRules, {
+    "type-enum": [2, "always", [
+      "build",
+      "chore",
+      "ci",
+      "docs",
+      "feature",
+      "fix",
+      "perf",
+      "refactor",
+      "revert",
+      "style",
+      "test",
+      "wip"
+    ]]
+  })
+})
