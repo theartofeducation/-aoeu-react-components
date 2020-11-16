169acd5f064525beb4eded7b4551c875e45021ae
Thomas Jean <thomas.jean486@gmail.com>
2020-11-16 10:03:19 -0500

    wip: test

---
+++ tests/logo.test.js
@@ -0,0 +1,11 @@
+import '@testing-library/jest-dom'
+
+import * as React from 'react'
+import { render, fireEvent, screen } from '@testing-library/react'
+import AOEULogo from '../components/logos'
+
+
+describe("AOEU Logo", () => {
+
+})
+
