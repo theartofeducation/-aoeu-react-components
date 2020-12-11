c9375b6f7405d0f9df6202162922068d522b6fe0
Bob Yexley <bob@yexley.net>
2020-12-11 15:58:54 -0500

    wip: meh

--- rollup.config.js
+++ rollup.config.js
@@ -2,28 +2,40 @@ import resolve from "@rollup/plugin-node-resolve"
 import babel from "@rollup/plugin-babel"
 // import image from "@rollup/plugin-image"
 import commonjs from "@rollup/plugin-commonjs"
-import url from "@rollup/plugin-url"
-import svgr from "@svgr/rollup"
+// import url from "@rollup/plugin-url"
+// import svgr from "@svgr/rollup"
 // import sass from "rollup-plugin-sass"
+// import autoExternal from "rollup-plugin-auto-external"
 import postcss from "rollup-plugin-postcss"
 
 export default function() {
-  const packageName = process.env.LERNA_PACKAGE_NAME
+  // const packageName = process.env.LERNA_PACKAGE_NAME
 
   return {
     input: "index.js",
-    output: {
-      file: "dist/index.js",
-      name: packageName,
-      format: "umd",
-      globals: {
-        "react": "React",
-        "react-dom": "ReactDOM",
-        "prop-types": "PropTypes"
+    output: [
+      // file: "dist/index.js",
+      // name: packageName,
+      // format: "umd",
+      // globals: {
+      //   "react": "React",
+      //   "react-dom": "ReactDOM",
+      //   "prop-types": "PropTypes"
+      // }
+      {
+        file: "dist/index.cjs.js",
+        format: "cjs",
+        sourcemap: true
+      },
+      {
+        file: "dist/index.es.js",
+        format: "es",
+        sourcemap: true
       }
-    },
+    ],
     preserveSymlinks: true,
     plugins: [
+      // autoExternal(),
       resolve({
         mainFields: ["main", "module"],
         extensions: [".js", ".jsx", ".json", ".svg", ".scss"],
@@ -42,13 +54,14 @@ export default function() {
       }),
       // image(),
       babel({
-        babelHelpers: "transform-runtime",
+        babelrc: false,
+        babelHelpers: "bundled",
         presets: ["@babel/env", "@babel/preset-react"],
-        // plugins: ["@babel/transform-runtime"],
+        plugins: [["transform-react-remove-prop-types", { mode: "wrap" }]],
         exclude: [/node_modules/]
       }),
-      url(),
-      svgr(),
+      // url(),
+      // svgr(),
       commonjs({
         include: [/node_modules/]
       })
