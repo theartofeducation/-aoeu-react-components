93c5af300ab649d8cf6b5180fe5346b22d2c2ed1
Bob Yexley <bob@yexley.net>
2020-12-16 07:53:32 -0500

    build: updated build for SSR compatibility (#21)
    * build: updated build for SSR compatibility
    * build: pr feedback - not generating source maps for production builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,85 +1,95 @@
-module.exports = {
-  entry: "./index.js",
-  output: {
-    filename: "index.js",
-    library: "aoeu-ui-common",
-    libraryTarget: "umd",
-    globalObject: "this"
-  },
-  resolve: {
-    extensions: [".js", ".jsx", ".json"],
-    modules: ["node_modules"]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: [
-          /node_modules/
-        ],
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              "@babel/preset-env",
-              "@babel/react"
-            ]
-          }
-        }
-      },
-      {
-        test: /\.(scss|sass|css)$/i,
-        oneOf: [
-          {
-            test: /\.module\.(scss|sass|css)$/,
-            use: [
-              "style-loader",
-              {
-                loader: "css-loader",
-                options: {
-                  modules: {
-                    localIdentName: "[local]-[hash:base64:7]",
-                    exportOnlyLocals: false
-                  }
-                }
-              },
-              "sass-loader"
-            ]
-          },
-          {
-            use: [
-              "style-loader",
-              "css-loader",
-              "sass-loader"
-            ]
-          }
-        ]
-      },
-      {
-        test: /\.svg$/,
-        use: ["@svgr/webpack", "url-loader"]
+const babelConfig = require("./babel.config.json")
+const production = process.env.NODE_ENV === "production"
+
+const entry = "./index.js"
+
+const output = {
+  filename: "index.js",
+  libraryTarget: "umd",
+  globalObject: "this"
+}
+
+const moduleConfig = {
+  rules: [
+    {
+      test: /\.(js|jsx)$/,
+      exclude: [
+        /node_modules/
+      ],
+      use: {
+        loader: "babel-loader",
+        options: babelConfig
       }
-    ]
-  },
-  plugins: [],
-  externals: {
-    "react": {
-      root: "React",
-      commonjs2: "react",
-      commonjs: "react",
-      amd: "react",
-      umd: "react"
     },
-    "react-dom": {
-      root: "ReactDOM",
-      commonjs2: "react-dom",
-      commonjs: "react-dom",
-      amd: "react-dom",
-      umd: "react-dom"
+    {
+      test: /\.(scss|sass|css)$/i,
+      oneOf: [
+        {
+          test: /\.module\.(scss|sass|css)$/,
+          use: [
+            "style-loader",
+            {
+              loader: "css-loader",
+              options: {
+                modules: {
+                  localIdentName: "[local]-[hash:base64:7]",
+                  exportOnlyLocals: false
+                }
+              }
+            },
+            "sass-loader"
+          ]
+        },
+        {
+          use: [
+            "style-loader",
+            "css-loader",
+            "sass-loader"
+          ]
+        }
+      ]
+    },
+    {
+      test: /\.svg$/,
+      use: ["@svgr/webpack", "url-loader"]
     }
+  ]
+}
+
+const externals = {
+  "react": {
+    root: "React",
+    commonjs2: "react",
+    commonjs: "react",
+    amd: "react",
+    umd: "react"
   },
-  stats: {
-    children: false,
-    modules: false
+  "react-dom": {
+    root: "ReactDOM",
+    commonjs2: "react-dom",
+    commonjs: "react-dom",
+    amd: "react-dom",
+    umd: "react-dom"
   }
 }
+
+const optimization = {
+  minimize: production
+}
+
+const stats = {
+  children: false,
+  modules: false
+}
+
+module.exports = Object.assign({}, {
+  entry,
+  output,
+  module: moduleConfig,
+  externals,
+  optimization,
+  stats,
+  ...(!production && {
+    devtool: "source-map"
+  })
+})
